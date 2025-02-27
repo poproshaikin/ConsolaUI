7bd482f619f8e6225c5eab364be9be99876545fb
poproshaikin <@poproshaikin>
2025-02-27 22:23:10 +0100

    Add Colors class to define default console colors

---
+++ Core/Colors.cs
@@ -0,0 +1,10 @@
+namespace ConsolaUI;
+
+internal static class Colors
+{
+    internal static readonly ConsoleColor DefaultBackgroundColor = Console.BackgroundColor;
+    internal static readonly ConsoleColor DefaultTextColor = Console.ForegroundColor;
+    
+    internal static readonly ConsoleColor DefaultSelectedBackgroundColor = ConsoleColor.Yellow;
+    internal static readonly ConsoleColor DefaultSelectedTextColor = ConsoleColor.Black;
+}
